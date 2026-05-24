201d76b6554f811d03e1c255c087c2d88884771a
foysalthecode <ahmedmdfoysal123@gmail.com>
2026-05-25 00:02:59 +0600

    validated user status

--- src/app/module/auth/auth.routes.ts
+++ src/app/module/auth/auth.routes.ts
@@ -5,4 +5,6 @@ const router = Router();
 
 router.post("/register", AuthController.registerPatient);
 
+router.post("/login", AuthController.lgoinUser);
+
 export const AuthRoutes = router;
--- src/app/module/auth/auth.service.ts
+++ src/app/module/auth/auth.service.ts
@@ -1,8 +1,13 @@
-import { Role } from "../../../generated/prisma/client";
+import { Role, UserStatus } from "../../../generated/prisma/client";
 import { auth } from "../../lib/auth";
 
 interface IRegisterPatientPayload {
   name: string;
+  email: string;
+  password: string;
+}
+
+interface ILoginUserPayload {
   email: string;
   password: string;
 }
@@ -32,6 +37,27 @@ const registerPatient = async (payload: IRegisterPatientPayload) => {
   return data;
 };
 
+const loginUser = async (payload: ILoginUserPayload) => {
+  const { email, password } = payload;
+  const data = await auth.api.signInEmail({
+    body: {
+      email,
+      password,
+    },
+  });
+
+  if (data.user.status === UserStatus.BLOCKED) {
+    throw new Error("Your account is blocked. Please contact support.");
+  }
+
+  if (data.user.isDeleted || data.user.status === UserStatus.DELETED) {
+    throw new Error("Your account has been deleted. Please contact support.");
+  }
+
+  return data;
+};
+
 export const AuthService = {
   registerPatient,
+  loginUser,
 };
