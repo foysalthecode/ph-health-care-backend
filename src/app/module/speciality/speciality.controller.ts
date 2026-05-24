59fb7f7234791ff3a8bfc73f445c06b4d60420bb
foysalthecode <ahmedmdfoysal123@gmail.com>
2026-05-24 20:03:38 +0600

    made function for try&catch block

--- src/app/module/speciality/speciality.controller.ts
+++ src/app/module/speciality/speciality.controller.ts
@@ -1,88 +1,52 @@
 import { Request, Response } from "express";
 import { specialityService } from "./speciality.service";
+import { catchAsync } from "../../shared/catchAsync";
 
-const createSpeciality = async (req: Request, res: Response) => {
-  try {
-    const payload = req.body;
-    const result = await specialityService.CreateSpeciality(payload);
+const createSpeciality = catchAsync(async (req: Request, res: Response) => {
+  const payload = req.body;
+  const result = await specialityService.CreateSpeciality(payload);
+  res.status(201).json({
+    success: true,
+    message: "Speciality Created Successfully",
+    data: result,
+  });
+});
 
-    res.status(201).json({
-      success: true,
-      message: "Speciality created successfully",
-      data: result,
-    });
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  } catch (err: any) {
-    console.log(err);
-    res.status(500).json({
-      success: false,
-      message: "Failed To create speciality",
-      error: err.message,
-    });
-  }
-};
+const GetAllSpeciality = catchAsync(async (req: Request, res: Response) => {
+  const speciality = await specialityService.GetAllSpeciality();
 
-const GetAllSpeciality = async (req: Request, res: Response) => {
-  try {
-    const speciality = await specialityService.GetAllSpeciality();
-    res.status(200).json({
-      success: true,
-      message: "Data Retirive successfully",
-      data: speciality,
-    });
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  } catch (err: any) {
-    console.log(err);
-    res.status(500).json({
-      success: false,
-      message: "Failed To create speciality",
-      error: err.message,
-    });
-  }
-};
+  res.status(200).json({
+    success: true,
+    message: "Data Retirive successfully",
+    data: speciality,
+  });
+});
 
-const DeleteSpeciality = async (req: Request, res: Response) => {
-  try {
-    const { id } = req.params;
-    const result = await specialityService.DeleteSpecialtiy(id as string);
+const DeleteSpeciality = catchAsync(async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const result = await specialityService.DeleteSpecialtiy(id as string);
 
-    res.status(200).json({
-      success: true,
-      message: "Speciality Deleted Successfully",
-      data: result,
-    });
+  res.status(200).json({
+    success: true,
+    message: "Speciality Deleted Successfully",
+    data: result,
+  });
+});
 
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  } catch (err: any) {
-    console.log(err);
-    res.status(500).json({
-      success: false,
-      message: "Failed To create speciality",
-      error: err.message,
-    });
-  }
-};
+const UpdateSpeciality = catchAsync(async (req: Request, res: Response) => {
+  const payload = req.body;
+  const { id } = req.params;
+  const result = await specialityService.UpdateSpeciality(
+    id as string,
+    payload,
+  );
 
-const UpdateSpeciality = async (req: Request, res: Response) => {
-  try {
-    const data = req.body;
-    const { id } = req.params;
-    const result = await specialityService.UpdateSpeciality(id as string, data);
-    res.status(200).json({
-      success: true,
-      message: "Updated Successfully",
-      data: result,
-    });
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  } catch (err: any) {
-    console.log(err);
-    res.status(500).json({
-      success: false,
-      message: "Failed To create speciality",
-      error: err.message,
-    });
-  }
-};
+  res.status(200).json({
+    success: true,
+    message: "Speciality Updated Successfully",
+    data: result,
+  });
+});
 
 export const specialityController = {
   createSpeciality,
---
+++ src/app/shared/catchAsync.ts
@@ -0,0 +1,17 @@
+import { NextFunction, Request, RequestHandler, Response } from "express";
+
+export const catchAsync = (Fn: RequestHandler) => {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      await Fn(req, res, next);
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    } catch (err: any) {
+      console.log(err);
+      res.status(500).json({
+        success: false,
+        message: "Failed To Fetch",
+        error: err.message,
+      });
+    }
+  };
+};
