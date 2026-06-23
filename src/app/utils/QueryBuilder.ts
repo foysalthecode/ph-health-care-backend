40ce0f633fbef3c014757ce7d21f8a50c4cab451
foysalthecode <ahmedmdfoysal123@gmail.com>
2026-06-23 20:17:53 +0600

    completed dynamic include

--- src/app/utils/QueryBuilder.ts
+++ src/app/utils/QueryBuilder.ts
@@ -340,6 +340,43 @@ export class QueryBuilder<
     return this;
   }
 
+  dynamicInclue(
+    includeConfig: Record<string, unknown>,
+    defaultInclude?: string[],
+  ): this {
+    if (this.selectFields) {
+      return this;
+    }
+
+    const result: Record<string, unknown> = {};
+
+    defaultInclude?.forEach((field) => {
+      if (includeConfig[field]) {
+        result[field] = includeConfig[field];
+      }
+    });
+
+    const includeParam = this.queryParams.includes as string | undefined;
+
+    if (includeParam && typeof includeParam === "string") {
+      const requstedRelations = includeParam
+        .split(",")
+        .map((relation) => relation.trim());
+      requstedRelations.forEach((relation) => {
+        if (includeConfig[relation]) {
+          result[relation] = includeConfig[relation];
+        }
+      });
+    }
+
+    this.query.include = {
+      ...(this.query.include as Record<string, unknown>),
+      ...result,
+    };
+
+    return this;
+  }
+
   private parseFilterValue(value: unknown): unknown {
     if (value === "true") {
       return true;
