f4e47b69bfc431b12752aa8bcc7899c8498c9032
foysalthecode <ahmedmdfoysal123@gmail.com>
2026-06-24 10:31:46 +0600

    Implemented Query Builder In Get All Doctors API

--- src/app/module/doctor/doctor.constant.ts
+++ src/app/module/doctor/doctor.constant.ts
@@ -1,3 +1,5 @@
+import { Prisma } from "../../../generated/prisma/client";
+
 export const doctorSearchableFields = [
   "name",
   "email",
@@ -21,3 +23,31 @@ export const doctorFilterableFields = [
   "specialties.specialty.title",
   "user.role",
 ];
+
+export const doctorIncludeConfig: Partial<
+  Record<
+    keyof Prisma.DoctorInclude,
+    Prisma.DoctorInclude[keyof Prisma.DoctorInclude]
+  >
+> = {
+  user: true,
+  specialities: {
+    include: {
+      speciality: true,
+    },
+  },
+  appointment: {
+    include: {
+      patient: true,
+      doctor: true,
+      prescription: true,
+    },
+  },
+  doctorSchedules: {
+    include: {
+      schedule: true,
+    },
+  },
+  prescription: true,
+  review: true,
+};
--- src/app/module/doctor/doctor.service.ts
+++ src/app/module/doctor/doctor.service.ts
@@ -7,8 +7,10 @@ import { QueryBuilder } from "../../utils/QueryBuilder";
 import { IQueryParams } from "../../interfaces/query.interface";
 import {
   doctorFilterableFields,
+  doctorIncludeConfig,
   doctorSearchableFields,
 } from "./doctor.constant";
+import { Doctor, Prisma } from "../../../generated/prisma/client";
 
 const getAllDoctors = async (query: IQueryParams) => {
   // const doctors = await prisma.doctor.findMany({
@@ -23,10 +25,32 @@ const getAllDoctors = async (query: IQueryParams) => {
   // });
   // return doctors;
 
-  const queryBuilder = new QueryBuilder(prisma.doctor, query, {
+  const queryBuilder = new QueryBuilder<
+    Doctor,
+    Prisma.DoctorWhereInput,
+    Prisma.DoctorInclude
+  >(prisma.doctor, query, {
     searchableFields: doctorSearchableFields,
     filterableFields: doctorFilterableFields,
   });
+
+  const result = await queryBuilder
+    .search()
+    .filter()
+    .where({
+      isDeleted: false,
+    })
+    .include({
+      user: true,
+      specialities: true,
+    })
+    .dynamicInclue(doctorIncludeConfig)
+    .paginate()
+    .sort()
+    .fields()
+    .execute();
+
+  return result;
 };
 
 const getDoctorById = async (id: string) => {
