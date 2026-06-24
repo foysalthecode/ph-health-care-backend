d20694618127ebbdfb4006a22ccd6dbc9919390b
foysalthecode <ahmedmdfoysal123@gmail.com>
2026-06-24 15:31:46 +0600

    implemented qs for parse

--- src/app/module/doctor/doctor.controller.ts
+++ src/app/module/doctor/doctor.controller.ts
@@ -12,7 +12,8 @@ const getAllDoctors = catchAsync(async (req: Request, res: Response) => {
     httpStatusCode: status.OK,
     success: true,
     message: "Doctors Retrive Successfully",
-    data: result,
+    data: result.data,
+    meta: result.meta,
   });
 });
 
