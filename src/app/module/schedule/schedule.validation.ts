c0c81a79bd64fb7684dbc9a170eadfbbaa1cd4d2
foysalthecode <ahmedmdfoysal123@gmail.com>
2026-06-27 23:18:22 +0600

    created zod validation for create & update schedule

--- src/app/module/schedule/schedule.validation.ts
+++ src/app/module/schedule/schedule.validation.ts
@@ -1,3 +1,44 @@
 import z from "zod";
 
-const createScheduleZodSchema = z.object
+const createScheduleZodSchema = z.object({
+  startDate: z.string().refine((date) => !isNaN(Date.parse(date)), {
+    message: "Invalid Date Format",
+  }),
+  endDate: z.string().refine((date) => !isNaN(Date.parse(date)), {
+    message: "Invalid Date Format",
+  }),
+  startTime: z
+    .string()
+    .refine((time) => /^([0-1]?[0-9]|2[0-3]):[0-5][0-9]$/.test(time), {
+      message: "Invalid Time Formate",
+    }),
+  endTime: z
+    .string()
+    .refine((time) => /^([0-1]?[0-9]|2[0-3]):[0-5][0-9]$/.test(time), {
+      message: "Invalid Time Formate",
+    }),
+});
+
+const updateScheduleZodSchema = z.object({
+  startDate: z.string().refine((date) => !isNaN(Date.parse(date)), {
+    message: "Invalid Date Format",
+  }),
+  endDate: z.string().refine((date) => !isNaN(Date.parse(date)), {
+    message: "Invalid Date Format",
+  }),
+  startTime: z
+    .string()
+    .refine((time) => /^([0-1]?[0-9]|2[0-3]):[0-5][0-9]$/.test(time), {
+      message: "Invalid Time Formate",
+    }),
+  endTime: z
+    .string()
+    .refine((time) => /^([0-1]?[0-9]|2[0-3]):[0-5][0-9]$/.test(time), {
+      message: "Invalid Time Formate",
+    }),
+});
+
+export const scheduleValidation = {
+  createScheduleZodSchema,
+  updateScheduleZodSchema,
+};
