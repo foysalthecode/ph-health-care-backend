7415b798661f022743b23a6c8fe4bf8793e721ff
foysalthecode <ahmedmdfoysal123@gmail.com>
2026-08-04 23:12:44 +0600

    schedule constant updated

--- src/app/module/schedule/schedule.constants.ts
+++ src/app/module/schedule/schedule.constants.ts
@@ -1,23 +1,23 @@
-// import { Prisma } from "../../../generated/prisma/client";
+import { Prisma } from "../../../generated/prisma/client";
 
-// export const scheduleFilterableFields = ["id", "startDateTime", "endDateTime"];
+export const scheduleFilterableFields = ["id", "startDateTime", "endDateTime"];
 
-// export const scheduleSearchableFields = ["id", "startDateTime", "endDateTime"];
+export const scheduleSearchableFields = ["id", "startDateTime", "endDateTime"];
 
-// export const scheduleInclueConfig: Partial<
-//   Record<
-//     keyof Prisma.ScheduleInclude,
-//     Prisma.ScheduleInclude[keyof Prisma.ScheduleInclude]
-//   >
-// > = {
-//   appointment: {
-//     include: {
-//       doctor: true,
-//       patient: true,
-//       payment: true,
-//       prescription: true,
-//       review: true,
-//     },
-//   },
-//   doctorSchedules: true,
-// };
+export const scheduleInclueConfig: Partial<
+  Record<
+    keyof Prisma.ScheduleInclude,
+    Prisma.ScheduleInclude[keyof Prisma.ScheduleInclude]
+  >
+> = {
+  appointment: {
+    include: {
+      doctor: true,
+      patient: true,
+      payment: true,
+      prescription: true,
+      review: true,
+    },
+  },
+  doctorSchedules: true,
+};
